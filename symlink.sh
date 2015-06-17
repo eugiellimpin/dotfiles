ceff3da496c3e882d82a94fb8d41e777c806f353
sham <eugielimpin@eugie-cfx-mbp.local>
2015-06-17 11:45:39 +0800

    Track .gitconfig
    Signed-off-by: sham <eugielimpin@eugie-cfx-mbp.local>

--- symlink.sh
+++ symlink.sh
@@ -5,3 +5,4 @@
 ln -sfhv ~/projects/dotfiles/vimrc ~/.vimrc
 ln -sfhv ~/projects/dotfiles/tmux.conf ~/.tmux.conf
 ln -sfhv ~/projects/dotfiles/config/fish/config.fish ~/.config/fish/config.fish
+ln -sfhv ~/projects/dotfiles/gitconfig ~/.gitconfig
