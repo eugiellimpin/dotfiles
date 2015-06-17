b550d4884e54e39894f70323286cbfe3d9ebfeb6
Eugie Limpin <eugiellimpin@gmail.com>
2015-06-17 11:39:18 +0800

    Symlink fish shell config file to ~
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- symlink.sh
+++ symlink.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# TODO: create variable for dotfiles src dir
+
 ln -sfhv ~/projects/dotfiles/vimrc ~/.vimrc
 ln -sfhv ~/projects/dotfiles/tmux.conf ~/.tmux.conf
+ln -sfhv ~/projects/dotfiles/config/fish/config.fish ~/.config/fish/config.fish
