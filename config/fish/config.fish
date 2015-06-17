c5829577c8ec5ee2775eac17fd18bd823cd43f5f
Eugie Limpin <eugiellimpin@gmail.com>
2015-06-17 11:43:23 +0800

    Create alias for rails. Add comment for clarity.
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,10 @@
 set PATH ~/.rbenv/bin $PATH
+
+# work-around for rbenv's `rbenv init` installation step
+# see https://github.com/sstephenson/rbenv/issues/195
 set PATH ~/.rbenv/shims $PATH
 rbenv rehash >/dev/null ^&1
 
 # ALIASES
 alias g="git"
+alias r="rails"
