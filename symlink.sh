ca0d988229d702e845b78d8c9867be0adc139456
Eugie Limpin <eugiellimpin@gmail.com>
2015-10-24 18:36:34 +0800

    Add teamocil layout files
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- symlink.sh
+++ symlink.sh
@@ -5,6 +5,7 @@
 ln -sfhv ~/projects/dotfiles/vimrc ~/.vimrc
 ln -sFfhv ~/projects/dotfiles/vim/after ~/.vim/after
 ln -sFfhv ~/projects/dotfiles/vim/ftdetect ~/.vim/ftdetect
+ln -sFfhv ~/projects/dotfiles/teamocil ~/.teamocil
 ln -sfhv ~/projects/dotfiles/tmux.conf ~/.tmux.conf
 ln -sfhv ~/projects/dotfiles/config.fish ~/.config/fish/config.fish
 ln -sfhv ~/projects/dotfiles/gitconfig ~/.gitconfig
