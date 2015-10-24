7922a1e1f2c543574e96ec6b9a253d48bae4b701
Eugie Limpin <eugiellimpin@gmail.com>
2015-10-24 18:16:32 +0800

    Add symlinks for .vim/after and .vim/ftdetect
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- symlink.sh
+++ symlink.sh
@@ -3,6 +3,8 @@
 # TODO: create variable for dotfiles src dir
 
 ln -sfhv ~/projects/dotfiles/vimrc ~/.vimrc
+ln -sFfhv ~/projects/dotfiles/vim/after ~/.vim/after
+ln -sFfhv ~/projects/dotfiles/vim/ftdetect ~/.vim/ftdetect
 ln -sfhv ~/projects/dotfiles/tmux.conf ~/.tmux.conf
 ln -sfhv ~/projects/dotfiles/config.fish ~/.config/fish/config.fish
 ln -sfhv ~/projects/dotfiles/gitconfig ~/.gitconfig
