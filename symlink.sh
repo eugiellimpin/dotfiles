7cea9b91799822eacf79d35d08b8a9920c8e608b
Eugie Limpin <eugiellimpin@gmail.com>
2015-10-24 23:47:02 +0800

    Use Solarized (dark) theme colors for tmux
    See https://github.com/altercation/solarized/tree/master/tmux
    
    - remove color definitions from tmux.conf
    
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- symlink.sh
+++ symlink.sh
@@ -7,5 +7,6 @@ ln -sFfhv ~/projects/dotfiles/vim/after ~/.vim/after
 ln -sFfhv ~/projects/dotfiles/vim/ftdetect ~/.vim/ftdetect
 ln -sFfhv ~/projects/dotfiles/teamocil ~/.teamocil
 ln -sfhv ~/projects/dotfiles/tmux.conf ~/.tmux.conf
+ln -sfhv ~/projects/dotfiles/tmuxcolors-solarized-dark.conf ~/.tmuxcolors-solarized-dark.conf
 ln -sfhv ~/projects/dotfiles/config.fish ~/.config/fish/config.fish
 ln -sfhv ~/projects/dotfiles/gitconfig ~/.gitconfig
