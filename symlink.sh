a3b64ada790b8bfbe719e3e8d33d7bec26a62b91
sham <eugielimpin@eugie-cfx-mbp.local>
2015-10-25 09:58:48 +0800

    Use variable for project directory in symlink.sh
    Signed-off-by: sham <eugielimpin@eugie-cfx-mbp.local>

--- symlink.sh
+++ symlink.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# TODO: create variable for dotfiles src dir
+PROJECT_DIR=$HOME/projects/dotfiles
 
-ln -sfhv ~/projects/dotfiles/vimrc ~/.vimrc
-ln -sFfhv ~/projects/dotfiles/vim/after ~/.vim/after
-ln -sFfhv ~/projects/dotfiles/vim/ftdetect ~/.vim/ftdetect
-ln -sFfhv ~/projects/dotfiles/teamocil ~/.teamocil
-ln -sfhv ~/projects/dotfiles/tmux.conf ~/.tmux.conf
-ln -sfhv ~/projects/dotfiles/tmuxcolors-solarized-dark.conf ~/.tmuxcolors-solarized-dark.conf
-ln -sfhv ~/projects/dotfiles/config.fish ~/.config/fish/config.fish
-ln -sfhv ~/projects/dotfiles/gitconfig ~/.gitconfig
+ln -sfFhv $PROJECT_DIR/vimrc ~/.vimrc
+ln -sFfhv $PROJECT_DIR/vim/after ~/.vim/after
+ln -sFfhv $PROJECT_DIR/vim/ftdetect ~/.vim/ftdetect
+ln -sFfhv $PROJECT_DIR/teamocil ~/.teamocil
+ln -sfFhv $PROJECT_DIR/tmux.conf ~/.tmux.conf
+ln -sfFhv $PROJECT_DIR/tmuxcolors-solarized-dark.conf ~/.tmuxcolors-solarized-dark.conf
+ln -sfFhv $PROJECT_DIR/config.fish ~/.config/fish/config.fish
+ln -sfFhv $PROJECT_DIR/gitconfig ~/.gitconfig
